b80d6beb43464dd720c667dcf91aedf5704e48bc
Arturo Rivera <a.rivera13@hotmail.com>
2019-02-22 03:10:40 -0800

    Creating new rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,7 +5,8 @@ class RoomList extends Component {
       super(props);
 
       this.state = {
-        rooms: []
+        rooms: [],
+        text: ''
       };
 
       this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -18,15 +19,27 @@ class RoomList extends Component {
       });
     }
 
+    createRoom() {
+      var textInput = document.getElementById("newRoomName");
+      var roomName = textInput.value;
+      this.props.firebase.database().ref('rooms/4').set(roomName);
+    }
+
     render() {
       return(
         <div>
             <h3>Room List</h3>
             {
                 this.state.rooms.map(room => {
-                return <li key={room.key}>{room.value}</li>
+                    return <li key={room.key}>{room.value}</li>
                 })
             }
+            <form onSubmit={() => this.createRoom()}>
+                Create New Room:<br />
+                <input type="text" id="newRoomName"></input>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
       )
 
